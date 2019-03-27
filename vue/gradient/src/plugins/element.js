e296eed0a74bed48e082d5933af1b0415b036d29
ma125120 <1418205177@qq.com>
2019-03-27 11:28:50 +0800

    init

---
+++ vue/gradient/src/plugins/element.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { Button, Slider, Input,
+  InputNumber, Switch,
+  Select,
+  Option,
+  OptionGroup,
+  Form,
+  FormItem,
+  ColorPicker, } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Slider)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Switch)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(ColorPicker);
